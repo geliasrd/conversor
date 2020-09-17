03e51123c87e803ca64a5054e28dfba6f605a002
Guilherme Elias <elias.doering@ufrgs.br>
2020-09-16 22:08:58 -0300

    #4: algoritmo base

--- main.f95
+++ main.f95
@@ -1,41 +1,34 @@
 program name
     implicit none
 
-    integer :: D !2147483647 maior inteiro cm 32bit
-    integer :: q
+    integer :: D, q, cont !2147483647 maior inteiro cm 32bit
+    integer(kind=1), DIMENSION(31) :: B ! maior espaço necessario pra numero decimal inteiro cm 32 bit
     integer(kind=1) :: r
-    character :: B*31, res*1 ! maior espaço necessario pra numero decimal inteiro cm 32 bit
-    ! salvar B como um vetor ou string?
 
-    print *, 'Digite o numero decimal desejado'
     read *, D
 
     ! configurando as variaveis pré loop
+
+
+    q = D
+    cont = 31
+
+    B = 0 ! limpar o espaço da memoria
+
     ! futuramente transformar em funçao/subrotina
-    q = D/2
-    r = MOD(D,2)
-    B = ''
-    B = TRIM(CHAR(r)) // TRIM(B)
-
-    ! objetivo: 
-    ! concatenar r com a string
-    ! '001'  ->  '1' + '001' - > '1001'
-    ! string = str(r) + string          no python
-    
-
     do while (q>0)
         r = MOD(q,2)
-        print *, r
+        ! print *, r
         q = q/2
-        res = CHAR(r)
-        B = TRIM(res) // TRIM(B)
-        print *, res
+        B(cont) = r
+        cont = cont - 1
     end do
 
-     ! continuar com o valor original do D
+
+    ! continuar com o valor original do D
 
 
-    print *,  D, 'escrito em binario é:', NEW_LINE(''), B
+    print *,D,'escrito em binario é:',NEW_LINE(''),B
 
 
     
